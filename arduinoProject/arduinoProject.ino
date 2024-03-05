471378e07dbe3abe02791141e2d78524df9477e8
Julian Schtutman <jschtutman@skyloom.co>
2024-03-05 18:41:24 -0300

    conectar a wifi, agrego carpeta de librerias

--- arduinoProject/arduinoProject.ino
+++ arduinoProject/arduinoProject.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <WiFiManager.h> 
+
+void setup() {
+  // Initialize serial port with baud rate of 9600 and 8N1 configuration
+  Serial.begin(9600, SERIAL_8N1);
+  WiFiManager wm;
+  bool res;
+
+  res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+  if(!res) {
+      Serial.println("Failed to connect");
+      // ESP.restart();
+  } 
+  else {
+      //if you get here you have connected to the WiFi    
+      Serial.println("connected...yeey :)");
+  }
+
+  // Print message to serial console
+  Serial.print("Hello World!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // This program does not require any code in the loop function
 }
